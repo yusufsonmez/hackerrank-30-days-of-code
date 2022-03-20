0233739acf3e0d8bb4d0224f19a9c8ca48e8f53c
yusufsonmez <yusufsonmez1995@gmail.com>
2022-03-21 02:32:58 +0300

    Day12 Solution

---
+++ Hackerrank-30daysofcode/Day12.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank_30daysofcode
+{
+   
+
+
+
+
+class Person
+    {
+        protected string firstName;
+        protected string lastName;
+        protected int id;
+
+        public Person() { }
+        public Person(string firstName, string lastName, int identification)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = identification;
+        }
+        public void printPerson()
+        {
+            Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+
+        }
+    }
+
+    class Student : Person
+    {
+        private int[] testScores;
+
+
+        public Student(string _firstName, string _lastName, int _idNumber, int[] _scores)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            id = _idNumber;
+            testScores = _scores;
+        }
+
+
+
+        public char Calculate()
+        {
+            int temp = 0;
+            foreach (var i in testScores)
+            {
+                temp = i + temp;
+            }
+
+
+            int grade = temp / testScores.Length;
+
+
+            if (grade < 40)
+            {
+                return 'T';
+            }
+            else if (grade >= 40 && grade < 55)
+            {
+                return 'D';
+            }
+            else if (grade >= 55 && grade < 70)
+            {
+                return 'P';
+            }
+            else if (grade >= 70 && grade < 80)
+            {
+                return 'A';
+            }
+            else if (grade >= 80 && grade < 90)
+            {
+                return 'E';
+            }
+            else if (grade >= 90 && grade <= 100)
+            {
+                return 'O';
+            }
+            else
+            {
+                return 'X';
+            }
+
+
+        }
+
+
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters: 
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+
+        /*	
+        *   Method Name: Calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your method here
+    }
+
+ 
+
+          
+     
+
+
+
+
+
+
+
+
+}
--- Hackerrank-30daysofcode/Program.cs
+++ Hackerrank-30daysofcode/Program.cs
@@ -10,8 +10,16 @@ namespace Hackerrank_30daysofcode
     {
         static void Main(string[] args)
         {
-            Day11 day11 = new Day11();
-            day11.SolutionDay11();
+    
+            Student day12 = new Student("Feyyaz", "Yigit", 2452, new int[] { 100,80});
+            day12.printPerson();
+            Console.WriteLine("Grade: " + day12.Calculate());
+            Console.ReadLine();
+
+
+
+            //Day11 day11 = new Day11();
+            //day11.SolutionDay11();
             // Sample input for day11
             //-1 1 - 1 0 0 0
             //0 - 1 0 0 0 0
@@ -20,8 +28,12 @@ namespace Hackerrank_30daysofcode
             //- 7 0 0 - 2 0 0
             //0 0 - 1 - 2 - 4 0
 
+
+
             //Day10 day10 = new Day10();
             //day10.SolutionDay10(5);
+
+
 
             //Day9 day9 = new Day9();
             //Console.WriteLine(day9.SolutionDay9(5));
